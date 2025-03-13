e76c85cb9b7cd3709a37590fc06d58c9c9e0bb05
VWDREAM <wanjiruvirginiah26@gmail.com>
2025-03-13 19:18:12 +0300

    array map lab

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    return tutorial
+      .split(" ") // Split string into an array of words
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize first letter of each word
+      .join(" "); // Join words back into a string
+  });
+};
+
